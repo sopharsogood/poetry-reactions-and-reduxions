9f947690325cb902502ba815c4188f11f12ceea5
sophie <justanothersophie@gmail.com>
2023-03-21 23:46:42 -0400

    Poem model contents attribute changed to content

--- db/migrate/20230322034331_create_poems.rb
+++ db/migrate/20230322034331_create_poems.rb
@@ -2,7 +2,7 @@ class CreatePoems < ActiveRecord::Migration[6.1]
   def change
     create_table :poems do |t|
       t.string :title
-      t.text :contents
+      t.text :content
       t.integer :author_id
 
       t.timestamps

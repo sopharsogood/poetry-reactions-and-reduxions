2329f88634d107616c0c1c0323db04070550cd9d
sophie <justanothersophie@gmail.com>
2023-03-22 13:52:58 -0400

    One-column table of poems for user page

--- poetry-app/src/Components/UserPoemsTable.js
+++ poetry-app/src/Components/UserPoemsTable.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
+import PoemTitleTD from './PoemTitleTD';
 
 class UserPoemsTable extends Component {
     constructor(props) {
         super(props);
     }
 
-    const 
-
+    // TODO: have redux set up this.props.poems
     render() {
-        
+        return <UserlessPoemRows poems={this.props.poems} />
     }
 }
+
+const UserlessPoemRows = ({ poems }) => (
+    <div>
+        { poems.map(poem => 
+            <tr>
+                <PoemTitleTD poemtitle={poem.title} />
+            </tr>    
+        )}
+    </div>
+)
 
 export default UserPoemsTable;

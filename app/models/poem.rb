08660db04e527c87be9adbbffc54c9f9a8ae0763
sophie <justanothersophie@gmail.com>
2023-03-21 23:59:09 -0400

    Model relationships

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,4 @@
 class Poem < ApplicationRecord
+    belongs_to :author, class_name: "User"
+    has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :poems, class_name: "Poem", foreign_key: "author_id"
+    has_many :comments, class_name: "Comment", foreign_key: "author_id"
+
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
 end

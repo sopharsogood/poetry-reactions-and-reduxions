d4e9dbf5c4ee667322dea0285dd941bedb62ddc0
sophie <justanothersophie@gmail.com>
2023-03-22 11:18:19 -0400

    UserPoemsTable component file

---
+++ poetry-app/src/Components/UserPoemsTable.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
+
+class UserPoemsTable extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+}
+
+export default UserPoemsTable;

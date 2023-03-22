a780d40ccb430540f3fa1aba94a6bd18b40bfc08
sophie <justanothersophie@gmail.com>
2023-03-22 00:32:05 -0400

    Resource routes (login/logout not included yet)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :poems do
+    resources :comments
+    get '/comments/:id/new', to: 'comments#new', as: 'new_comment_reply'
+    post '/comments/:id/new', to: 'comments#create'
+  end
+  resources :users
+
 end

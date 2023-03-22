065011a5ff7a06b13dec81ba087b0d12ba0b11af
sophie <justanothersophie@gmail.com>
2023-03-22 11:12:59 -0400

    PoemTitleTD component

---
+++ poetry-app/src/Components/PoemTitleTD.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PoemTitleTD extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render {
+        return(
+            <td>
+                {this.props.poemtitle}
+            </td>
+        )
+    }
+}
+
+export default PoemTitleTD;

0f18c1e51138975c6aebba16b3f83d7f951fa483
sophie <justanothersophie@gmail.com>
2023-03-23 11:26:53 -0400

    Show user API controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         users = User.all
         render json: UserSerializer.new(users).to_serialized_json
     end
+
+    def show
+        user = User.find(params[:id])
+        render json: UserSerializer.new(users).to_serialized_json
+    end
 end

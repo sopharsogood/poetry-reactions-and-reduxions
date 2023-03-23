a3b879a1afc66d7848313796146d3af0ad8659c0
sophie <justanothersophie@gmail.com>
2023-03-23 16:22:33 -0400

    Create session (login) API action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
-
-
-
-
+    def create
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: {user: user}
+        else
+            render json: { status 401 }
+        end
+    end
 
 end

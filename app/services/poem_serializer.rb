242fbefa8089f685eabaa9fb981982ba1041c9b8
sophie <justanothersophie@gmail.com>
2023-03-23 11:37:09 -0400

    No need to send all comment info with poem; can get separately if on show page

--- app/services/poem_serializer.rb
+++ app/services/poem_serializer.rb
@@ -5,10 +5,7 @@ class PoemSerializer
 
     def to_serialized_json
         @poem.to_json(:include => {
-            :author => {:only => [:username, :id]},
-            :comments => {:include => {
-                :author => {:only => [:username, id]}
-                }}
+            :author => {:only => [:username, :id]}
         }, :except => [:created_at, :updated_at])
     end
 end 

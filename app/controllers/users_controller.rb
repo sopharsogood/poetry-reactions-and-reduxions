7b4ef12430ae1de89872a9ab2e7eccdd6aa0f158
sophie <justanothersophie@gmail.com>
2023-03-23 16:29:40 -0400

    switched cors permitted localhost from 3001 to 4000

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: UserSerializer.new(user).to_serialized_json
     end
+
+    def create
+        user = User.create!(username: params[:username], password: params[:password])
+        if user
+            session[:user_id] = user.id
+            render json: {user: user}
+        else 
+            render json: { status: 500 }
+        end
+    end
 end

bda6e01ff14a6c0a7a9d002b31c8955d026b8d5e
sophie <justanothersophie@gmail.com>
2023-03-23 11:34:58 -0400

    First pass at poem serializer with all info needed for poem show page in app

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,8 +1,6 @@
 class PoemsController < ApplicationController
     def index
         poems = Poem.all
-        render json: poems.to_json(:include => {
-            :author => {:only => [:username, :id]}
-        })
+        render json: PoemSerializer.new(poems).to_serialized_json
     end
 end
---
+++ app/services/poem_serializer.rb
@@ -0,0 +1,14 @@
+class PoemSerializer
+    def initialize(poem)
+        @poem = poem
+    end
+
+    def to_serialized_json
+        @poem.to_json(:include => {
+            :author => {:only => [:username, :id]},
+            :comments => {:include => {
+                :author => {:only => [:username, id]}
+                }}
+        }, :except => [:created_at, :updated_at])
+    end
+end 

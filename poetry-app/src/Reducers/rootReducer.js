bf998255c9a5a106cbdcd333e06b33f98b1b6db3
sophie <justanothersophie@gmail.com>
2023-03-24 10:45:46 -0400

    Non-existent imports removed

--- poetry-app/src/Reducers/rootReducer.js
+++ poetry-app/src/Reducers/rootReducer.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux'
 import usersReducer from './usersReducer'
-import poemsReducer from './poemsReducer'
-import commentsReducer from './commentsReducer'
 
 const rootReducer = combineReducers({
     users: usersReducer

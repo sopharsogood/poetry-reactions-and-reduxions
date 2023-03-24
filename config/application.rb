3fa54c75f266f2278f186dbbe45d257013534a2d
sophie <justanothersophie@gmail.com>
2023-03-24 11:00:12 -0400

    Disabled cookies in config/application pending bugfixing

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ Bundler.require(*Rails.groups)
 module PoetryReactionsAndReduxions
   class Application < Rails::Application
     config.api_only = true
-    config.middleware.use ApplicationDispatch::Cookies
-    config.middleware.use ApplicationDispatch::Session::CookieStore, key: 666a62e377b1d0c87404d46ae7c9f7165e4afdbd0262e7986bba499ede5c709f19621773070f5ced3239b93ea58d5603afa393938efad3be5263d59c353739f8
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1

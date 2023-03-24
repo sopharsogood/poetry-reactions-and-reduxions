52aeeca357d04f7720133e3e49d2184d20d7ac0b
sophie <justanothersophie@gmail.com>
2023-03-24 10:46:46 -0400

    rootReducer import path corrected

--- poetry-app/src/index.js
+++ poetry-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
+import rootReducer from './Reducers/rootReducer';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

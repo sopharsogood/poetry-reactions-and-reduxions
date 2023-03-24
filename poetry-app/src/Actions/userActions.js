42798408dfe21be5a3499f8901e4d787097ab423
sophie <justanothersophie@gmail.com>
2023-03-24 09:56:57 -0400

    attempt_register function

--- poetry-app/src/Actions/userActions.js
+++ poetry-app/src/Actions/userActions.js
@@ -20,3 +20,24 @@ export function attempt_login(user) {
         )
     }
 }
+
+export function attempt_register(user) {
+    return dispatch => {
+        fetch("/users/new", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        },
+            { withCredentials: true }
+        )
+            .then(resp => resp.json())
+            .then(response_user => {
+                if (response_user) {
+                    dispatch({ type: LOGIN, user: response_user })
+                } 
+            }
+        )
+    }
+}

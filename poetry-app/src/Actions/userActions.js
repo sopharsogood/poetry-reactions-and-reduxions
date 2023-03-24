e5da82e3989eb3d278e7c3afda486ea75b72bb0c
sophie <justanothersophie@gmail.com>
2023-03-24 09:58:49 -0400

    attempt_logout function

--- poetry-app/src/Actions/userActions.js
+++ poetry-app/src/Actions/userActions.js
@@ -41,3 +41,24 @@ export function attempt_register(user) {
         )
     }
 }
+
+export function attempt_logout() {
+    return dispatch => {
+        fetch("/logout", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        },
+            { withCredentials: true }
+        )
+            .then(resp => resp.json())
+            .then(response => {
+                if (response) {
+                    dispatch({ type: LOGOUT })
+                } 
+            }
+        )
+    }
+}

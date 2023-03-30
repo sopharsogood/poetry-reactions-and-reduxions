549f2235d2b6fb9c38cdcf148c1a9508e95fc386
sophie <justanothersophie@gmail.com>
2023-03-30 19:31:12 -0400

    Reducer actions for loading showpoem

--- poetry-app/src/Reducers/commentsReducer.js
+++ poetry-app/src/Reducers/commentsReducer.js
@@ -1,5 +1,7 @@
 function commentsReducer(state = [], action) {
     switch (action.type) {
+        case LOAD_SHOWPOEM_AND_COMMENTS:
+            return action.showpoem.comments
         default:
             return state
     }
--- poetry-app/src/Reducers/showpoemReducer.js
+++ poetry-app/src/Reducers/showpoemReducer.js
@@ -1,5 +1,13 @@
+import { LOAD_SHOWPOEM_AND_COMMENTS } from "../Actions/actionTypes";
+
 function showpoemReducer(state = {}, action) {
     switch (action.type) {
+        case LOAD_SHOWPOEM_AND_COMMENTS:
+            return {
+                title: action.showpoem.title,
+                content: action.showpoem.content,
+                author: action.showpoem.author
+            }
         default:
             return state
     }

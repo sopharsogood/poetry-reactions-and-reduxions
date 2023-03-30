37e106aeedc1fd850b709af43369d476b9f06185
sophie <justanothersophie@gmail.com>
2023-03-30 19:13:59 -0400

    Function to query server for poem Show route

--- poetry-app/src/Actions/showpoemActions.js
+++ poetry-app/src/Actions/showpoemActions.js
@@ -1,1 +1,10 @@
 import { LOAD_SHOWPOEM_AND_COMMENTS } from "./actionTypes";
+
+export function fetchShowpoemAndComments(showpoemId) {
+    const showpoemDataUrl = "/poems/" + showpoemId
+    const showpoemData = fetch(showpoemDataUrl, {
+            method: "GET"
+        })
+            .then(resp => resp.json())
+    return showpoemData
+}

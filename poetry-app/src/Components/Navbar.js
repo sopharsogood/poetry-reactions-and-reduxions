bba64267ff7b31189e2c7f49375744922918886b
sophie <justanothersophie@gmail.com>
2023-04-05 00:06:14 -0400

    Navbar JSX for clickable form-showing buttons

--- poetry-app/src/Components/Navbar.js
+++ poetry-app/src/Components/Navbar.js
@@ -6,16 +6,21 @@ import { connect } from 'react-redux';
 class Navbar extends Component {
     constructor(props) {
         super(props);
+        this.state.showingForm = ""
     }
+
+
 
     render() {
         let loginLogoutNav
         if (this.props.session.userid > 0) {
-            loginLogoutNav = <div>
-                    <UsernameLink username={this.props.session.username} userid={this.props.session.userid} /> | Log Out
-                </div>
+            loginLogoutNav = <span>
+                    <UsernameLink username={this.props.session.username} userid={this.props.session.userid} /> | <span onClick={handleOnClickNewPoem}>New Poem</span> | Log Out
+                </span>
         } else {
-            loginLogoutNav = "Login | Register"
+            loginLogoutNav = <span>
+                <span onClick={handleOnClickLogin}>Login</span> | <span onClick={handleOnClickRegister}>Register</span>
+            </span>
         }
         return(
             <h5>

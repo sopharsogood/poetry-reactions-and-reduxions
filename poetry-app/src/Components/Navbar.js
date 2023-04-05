f0f66440ea7f8d49546607e38d07ce816b641572
sophie <justanothersophie@gmail.com>
2023-04-04 23:40:40 -0400

    Navbar reads session state from store; Navbar JSX updates

--- poetry-app/src/Components/Navbar.js
+++ poetry-app/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom";
 import UsernameLink from './UsernameLink';
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
     constructor(props) {
@@ -9,21 +10,27 @@ class Navbar extends Component {
 
     render() {
         let loginLogoutNav
-        if (this.props.is_logged_in) {
+        if (this.props.session.userid > 0) {
             loginLogoutNav = <div>
-                    <UsernameLink username={this.props.user.username} userid={this.props.user.userid} /> | Log Out
+                    <UsernameLink username={this.props.session.username} userid={this.props.session.userid} /> | Log Out
                 </div>
         } else {
             loginLogoutNav = "Login | Register"
         }
         return(
             <h5>
-                <NavLink to="/">
-                    Home
-                </NavLink> | {loginLogoutNav}
+                {loginLogoutNav}
             </h5>
         )
     }
 }
 
-export default Navbar;
+const mapStateToProps = state => {
+    return {
+        session: state.session
+    };
+};
+
+export default connect(
+    mapStateToProps
+)(Navbar);

ec59e17c0a96c7f4df357f0fdef7a883dbe760ab
sophie <justanothersophie@gmail.com>
2023-04-05 01:01:48 -0400

    NavbarFormContainer decides which form to render based on props

--- poetry-app/src/Components/NavbarFormContainer.js
+++ poetry-app/src/Components/NavbarFormContainer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import LoginForm from './LoginForm';
+import NewPoemForm from './NewPoemForm';
+import RegisterForm from './RegisterForm';
 
 class NavbarFormContainer extends Component {
     constructor(props) {
@@ -7,7 +10,18 @@ class NavbarFormContainer extends Component {
     }
 
     render() {
+        let formToShow
+        if (this.props.session.userid > 0 && this.props.showingForm == "NewPoem") {
+            formToShow = <NewPoemForm />
+        } else if (this.props.session.userid = 0 && this.props.showingform == "Login") {
+            formToShow = <LoginForm />
+        } else if (this.props.session.userid = 0 && this.props.showingform == "Register") {
+            formToShow = <RegisterForm />
+        }
 
+        return(
+            {formToShow}
+        )
     }
 }
     

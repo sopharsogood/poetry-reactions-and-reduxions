c37226940792ed291e219f6caeca925c8654f7c4
sophie <justanothersophie@gmail.com>
2023-04-05 01:25:02 -0400

    Form fields value properties changed to read from correct value of formData prop instead of entire formData

--- poetry-app/src/Components/PoemFields.js
+++ poetry-app/src/Components/PoemFields.js
@@ -8,8 +8,8 @@ class PoemFields extends Component {
     render() {
         return(
             <p>
-                Title: <input type="text" name="title" id="title" onChange={event => this.props.handleChange(event)} value={this.props.formData} /> <br />
-                Poem: <input type="textarea" name="content" id="content" onChange={event => this.props.handleChange(event)} value={this.props.formData} />
+                Title: <input type="text" name="title" id="title" onChange={event => this.props.handleChange(event)} value={this.props.formData.title} /> <br />
+                Poem: <input type="textarea" name="content" id="content" onChange={event => this.props.handleChange(event)} value={this.props.formData.content} />
             </p>
         )
     }
--- poetry-app/src/Components/UsernamePasswordFields.js
+++ poetry-app/src/Components/UsernamePasswordFields.js
@@ -8,8 +8,8 @@ class UsernamePasswordFields extends Component {
     render() {
         return(
             <p>
-                Username: <input type="text" name="username" id="username" onChange={event => this.props.handleChange(event)} value={this.props.formData} /> <br />
-                Password: <input type="password" name="password" id="password" onChange={event => this.props.handleChange(event)} value={this.props.formData} />
+                Username: <input type="text" name="username" id="username" onChange={event => this.props.handleChange(event)} value={this.props.formData.username} /> <br />
+                Password: <input type="password" name="password" id="password" onChange={event => this.props.handleChange(event)} value={this.props.formData.password} />
             </p>
         )
     }
